b4f6cf1c37eec4d6f1daba0bdd18e036e71d18ee
ArneVDE <r0704140@student.thomasmore.be>
2019-12-09 11:13:06 +0100

    Navbar met routing

--- RomeFrontend/src/app/app.module.ts
+++ RomeFrontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TaskComponent } from './task/task.component';
+
+const appRoutes: Routes = [
+
+  { path: 'Opdrachten', component :  TaskComponent},
+  { path: 'Profiel', component : ProfileComponent},
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    TaskComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
