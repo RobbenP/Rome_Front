65db016c01d172239159ad25cce6912499f4e418
Ruben Tuytens <tuytensruben@gmail.com>
2019-12-09 14:59:38 +0100

    login

--- RomeFrontend/src/app/login/login/login.component.ts
+++ RomeFrontend/src/app/login/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticateService } from 'src/app/services/authenticate.service';
+import { Userlogin } from 'src/app/models/userlogin.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: Userlogin = new Userlogin('','');
+  constructor(private _authenticateService: AuthenticateService, private _router: Router) { }
 
-  constructor() { }
-
+  onSubmit(){
+    this._authenticateService.authenticate(this.model).subscribe(result =>
+      {
+        localStorage.setItem("token",result.token);
+        this._router.navigate(['']);
+      })
+  }
   ngOnInit() {
   }
 
