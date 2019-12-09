3dfff75175a448833827f127e8569c2799a9a098
Robben P <robben.praet@gmail.com>
2019-12-09 14:17:31 +0100

    werken aan available tasks

--- RomeFrontend/src/app/student/available-tasks/available-tasks.component.ts
+++ RomeFrontend/src/app/student/available-tasks/available-tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { NavigationExtras, Router } from "@angular/router";
 import { AssignmentService } from "src/app/services/assignment.service";
+import { Observable } from 'rxjs';
+import { Assignment } from 'src/app/models/assignment.model';
 
 @Component({
   selector: "app-available-tasks",
@@ -8,10 +10,13 @@ import { AssignmentService } from "src/app/services/assignment.service";
   styleUrls: ["./available-tasks.component.css"]
 })
 export class AvailableTasksComponent implements OnInit {
+  allAssignements:Observable<Assignment[]>;
   constructor(
     private router: Router,
     private assignmentService: AssignmentService
-  ) {}
+  ) {
+    this.allAssignements=assignmentService.getAssignments();
+  }
 
   details(assignmentId: number) {
     let navExtras: NavigationExtras = {
