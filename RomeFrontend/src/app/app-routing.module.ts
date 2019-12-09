3b925720900ae02b3a8a01814258c0fdbc645ecc
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-09 14:45:21 +0100

    delete files

--- RomeFrontend/src/app/app-routing.module.ts
+++ RomeFrontend/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   { path: 'registreer', component : RegisterComponent},
   { path: 'registreer/bedrijf', component : RegisterCompanyComponent},
   { path: 'taakToevoegen', component : AddTaskComponent},
-  {path: 'takenlijstStudent', component:AvailableTasksComponent}
+  { path: 'takenlijstStudent', component:AvailableTasksComponent}
   ]; 
 
   @NgModule({
--- RomeFrontend/src/app/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export class User {
-    public userID: number;
-    public username: string;
-    public email: string;
-    public password: string;
-}
