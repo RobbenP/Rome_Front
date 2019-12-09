43654af803bab13fa93be22d07e3735bfa0e4559
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-09 10:30:48 +0100

    making components

--- RomeFrontend/src/app/app.module.ts
+++ RomeFrontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AvailableTasksComponent } from './student/available-tasks/available-tasks.component';
+import { TaskDetailsComponent } from './student/available-tasks/task-details/task-details.component';
+import { ListUsersComponent } from './admin/list-users/list-users.component';
+import { ListTasksComponent } from './admin/list-tasks/list-tasks.component';
+import { AddTaskComponent } from './company/list-tasks/add-task/add-task.component';
+import { DetailTaskComponent } from './company/list-tasks/detail-task/detail-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvailableTasksComponent,
+    TaskDetailsComponent,
+    ListUsersComponent,
+    ListTasksComponent,
+    AddTaskComponent,
+    DetailTaskComponent
   ],
   imports: [
     BrowserModule
