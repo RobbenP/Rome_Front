49e0cd1e3c4251c107dc606366ca627779a1041f
Robben P <robben.praet@gmail.com>
2019-12-09 13:42:22 +0100

    start taskservice

--- RomeFrontend/src/app/services/assignment.service.ts
+++ RomeFrontend/src/app/services/assignment.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Assignment } from '../models/assignment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssignmentService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAssignments():Observable<Assignment[]>{
+    return this.http.get<Assignment[]>("https://localhost:5001/api/Assignments")
+  }
+  addAssignement(assignment:Assignment){
+    return this.http.post("https://localhost:5001/api/Assignments", assignment)
+  }
+
 }
--- RomeFrontend/src/app/student/available-tasks/available-tasks.component.ts
+++ RomeFrontend/src/app/student/available-tasks/available-tasks.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
-import { AssignmentService } from 'src/app/services/assignment.service';
+import { Component, OnInit } from "@angular/core";
+import { NavigationExtras, Router } from "@angular/router";
+import { AssignmentService } from "src/app/services/assignment.service";
 
 @Component({
-  selector: 'app-available-tasks',
-  templateUrl: './available-tasks.component.html',
-  styleUrls: ['./available-tasks.component.css']
+  selector: "app-available-tasks",
+  templateUrl: "./available-tasks.component.html",
+  styleUrls: ["./available-tasks.component.css"]
 })
 export class AvailableTasksComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private assignmentService: AssignmentService
+  ) {}
 
-  constructor(private router:Router, private assignmentService:AssignmentService) { }
-
-  details(pollid: number) {
+  details(assignmentId: number) {
     let navExtras: NavigationExtras = {
       queryParams: {
-        pollid: pollid
+        assignmentId: assignmentId
       }
     };
-    this.router.navigate(["/detailsPoll"], navExtras);
+    this.router.navigate(["/detailsAssignement"], navExtras);
   }
 
-
-  ngOnInit() {
+  signup(assignmentId: number) {
+    
   }
 
+  ngOnInit() {}
 }
