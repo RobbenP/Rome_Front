5d9eaab5472b67fca655038787937ff787091506
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-09 11:26:55 +0100

    routing

--- RomeFrontend/src/app/app.module.ts
+++ RomeFrontend/src/app/app.module.ts
@@ -5,14 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { TaskComponent } from './task/task.component';
-
-const appRoutes: Routes = [
-
-  { path: 'Opdrachten', component :  TaskComponent},
-  { path: 'Profiel', component : ProfileComponent},
-  ];
+import { LoginComponent} from './login/login/login.component';
+import { RegisterComponent } from './register/register/register.component';
 import { AvailableTasksComponent } from './student/available-tasks/available-tasks.component';
 import { TaskDetailsComponent } from './student/available-tasks/task-details/task-details.component';
 import { ListUsersComponent } from './admin/list-users/list-users.component';
@@ -20,17 +14,24 @@ import { ListTasksComponent } from './admin/list-tasks/list-tasks.component';
 import { AddTaskComponent } from './company/list-tasks/add-task/add-task.component';
 import { DetailTaskComponent } from './company/list-tasks/detail-task/detail-task.component';
 
+const appRoutes: Routes = [
+
+  { path: 'login', component :  LoginComponent},
+  { path: 'register', component : RegisterComponent},
+  ];
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
-    TaskComponent,
     AvailableTasksComponent,
     TaskDetailsComponent,
     ListUsersComponent,
     ListTasksComponent,
     AddTaskComponent,
-    DetailTaskComponent
+    DetailTaskComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
