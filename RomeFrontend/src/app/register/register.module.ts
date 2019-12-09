b33ca5aa2bb1a315e90a3c5d804f0a5dd0a4a1e7
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-09 12:54:37 +0100

    puur frontend register and login form

--- RomeFrontend/src/app/register/register.module.ts
+++ RomeFrontend/src/app/register/register.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
+import { RegisterStudentComponent } from './register-student/register-student.component';
+import { RegisterCompanyComponent } from './register-company/register-company.component';
 
 
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, RegisterStudentComponent, RegisterCompanyComponent],
   imports: [
     CommonModule
   ]
