05cacfe59409be89fccd0b29264a5e98ccff53d2
ArneVDE <r0704140@student.thomasmore.be>
2019-12-09 13:25:47 +0100

    model and form task toevoegen

--- RomeFrontend/src/app/app.module.ts
+++ RomeFrontend/src/app/app.module.ts
@@ -18,7 +18,8 @@ const appRoutes: Routes = [
 
   { path: 'login', component :  LoginComponent},
   { path: 'register', component : RegisterComponent},
-  ];
+  { path: 'addTask', component : AddTaskComponent},
+  ]; 
 
 
 @NgModule({
