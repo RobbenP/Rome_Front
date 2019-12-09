ba6e882d6e6f2a7639d7a734eba79030c4bef35f
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-09 14:01:08 +0100

    routing file

---
+++ RomeFrontend/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent} from './login/login/login.component';
+import { RegisterComponent } from './register/register/register.component';
+import { AvailableTasksComponent } from './student/available-tasks/available-tasks.component';
+import { TaskDetailsComponent } from './student/available-tasks/task-details/task-details.component';
+import { ListUsersComponent } from './admin/list-users/list-users.component';
+import { ListTasksComponent } from './admin/list-tasks/list-tasks.component';
+import { AddTaskComponent } from './company/list-tasks/add-task/add-task.component';
+import { DetailTaskComponent } from './company/list-tasks/detail-task/detail-task.component';
+import { RegisterCompanyComponent } from './register/register-company/register-company.component'
+
+const routes: Routes = [
+
+  { path: 'login', component :  LoginComponent},
+  { path: 'registreer', component : RegisterComponent},
+  { path: 'registreer/bedrijf', component : RegisterCompanyComponent},
+  { path: 'taakToevoegen', component : AddTaskComponent},
+  ]; 
+
+  @NgModule({
+    imports: [
+      RouterModule.forRoot(
+        routes,
+        { enableTracing: true } // <-- debugging purposes only
+      )
+    ],
+    exports: [
+      RouterModule
+    ]
+  })
+  export class AppRoutingModule {}
--- RomeFrontend/src/app/app.module.ts
+++ RomeFrontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { LoginComponent} from './login/login/login.component';
@@ -14,16 +14,7 @@ import { ListUsersComponent } from './admin/list-users/list-users.component';
 import { ListTasksComponent } from './admin/list-tasks/list-tasks.component';
 import { AddTaskComponent } from './company/list-tasks/add-task/add-task.component';
 import { DetailTaskComponent } from './company/list-tasks/detail-task/detail-task.component';
-import { RegisterCompanyComponent } from './register/register-company/register-company.component'
-
-const appRoutes: Routes = [
-
-  { path: 'login', component :  LoginComponent},
-  { path: 'registreer', component : RegisterComponent},
-  { path: 'registreer/bedrijf', component : RegisterCompanyComponent},
-  { path: 'taakToevoegen', component : AddTaskComponent},
-  ]; 
-
+import { RegisterCompanyComponent } from './register/register-company/register-company.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +34,7 @@ const appRoutes: Routes = [
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
