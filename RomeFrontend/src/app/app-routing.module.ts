c69babc6de3d1e12b25db7f51727ecf63ca67b06
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-10 08:46:37 +0100

    routing

--- RomeFrontend/src/app/app-routing.module.ts
+++ RomeFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-<<<<<<< HEAD
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login/login.component";
 import { RegisterComponent } from "./register/register/register.component";
@@ -12,37 +11,17 @@ import { ListTasksComponent } from "./admin/list-tasks/list-tasks.component";
 import { AddTaskComponent } from "./company/list-tasks/add-task/add-task.component";
 import { DetailTaskComponent } from "./company/list-tasks/detail-task/detail-task.component";
 import { RegisterCompanyComponent } from "./register/register-company/register-company.component";
+import { RegisterStudentComponent } from './register/register-student/register-student.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "registreer", component: RegisterComponent },
   { path: "registreer/bedrijf", component: RegisterCompanyComponent },
+  { path: "registreer/student", component: RegisterStudentComponent},
   { path: "taakToevoegen", component: AddTaskComponent },
   { path: "takenlijstStudent", component: AvailableTasksComponent },
-  { path: "detailsTaakStudent", component: TaskDetailsComponent }
-=======
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { RegisterComponent } from './register/register/register.component';
-import { AvailableTasksComponent } from './student/available-tasks/available-tasks.component';
-import { TaskDetailsComponent } from './student/available-tasks/task-details/task-details.component';
-import { ListUsersComponent } from './admin/list-users/list-users.component';
-import { ListTasksComponent } from './admin/list-tasks/list-tasks.component';
-import { AddTaskComponent } from './company/list-tasks/add-task/add-task.component';
-import { DetailTaskComponent } from './company/list-tasks/detail-task/detail-task.component';
-import { RegisterCompanyComponent } from './register/register-company/register-company.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterStudentComponent } from './register/register-student/register-student.component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'registreer', component: RegisterComponent },
-  { path: 'registreer/bedrijf', component: RegisterCompanyComponent },
-  { path: 'registreer/student', component:RegisterStudentComponent},
-  { path: 'taakToevoegen', component: AddTaskComponent },
+  { path: "detailsTaakStudent", component: TaskDetailsComponent },
   { path: 'takenlijstStudent', component: AvailableTasksComponent }
->>>>>>> 179772bf9327c960cbcb478e1a6aff151ff37312
 ];
 
 @NgModule({
@@ -52,14 +31,8 @@ const routes: Routes = [
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-<<<<<<< HEAD
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
-=======
   exports: [
     RouterModule
   ]
 })
 export class AppRoutingModule { }
->>>>>>> 179772bf9327c960cbcb478e1a6aff151ff37312
