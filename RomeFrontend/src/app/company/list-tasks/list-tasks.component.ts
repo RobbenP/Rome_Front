8f1eab3aa1415ffc584663a440b8d1c3862ef0f9
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-10 11:42:28 +0100

    bedrijf

--- RomeFrontend/src/app/company/list-tasks/list-tasks.component.ts
+++ RomeFrontend/src/app/company/list-tasks/list-tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Assignment } from 'src/app/models/assignment.model';
+import { Router, NavigationExtras } from '@angular/router';
+import { AssignmentService } from 'src/app/services/assignment.service';
 
 @Component({
   selector: 'app-list-tasks',
@@ -7,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTasksComponent implements OnInit {
 
-  constructor() { }
+  allAssignements: Assignment[];
+  constructor(
+    private router: Router,
+    private assignmentService: AssignmentService
+  ) {
+    //this.allAssignements = assignmentService.getAssignments();
+    assignmentService.getAssignments().subscribe(r => {
+      this.allAssignements = r;
+      console.log(r);
+    });
+  }
+
+  details(assignmentId: number) {
+    let navExtras: NavigationExtras = {
+      queryParams: {
+        assignmentId: assignmentId
+      }
+    };
+    this.router.navigate(["bedrijf/detailsTaak"], navExtras);
+  }
+
+  signup(assignmentId: number) {}
 
   ngOnInit() {
   }
