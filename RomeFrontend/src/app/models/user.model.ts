f22482d0fe3860335d7f9376278ce7bae6f49e8e
Ruben Tuytens <tuytensruben@gmail.com>
2019-12-10 10:28:22 +0100

    registreer werkt

--- RomeFrontend/src/app/models/user.model.ts
+++ RomeFrontend/src/app/models/user.model.ts
@@ -1,15 +1,4 @@
 export class User {
-<<<<<<< HEAD
     constructor(public userID: number, public username: string, public email: string,
         public password: string, public roleid: number, public token: string, public companyID: number, public studentID : number){}
-=======
-  constructor(
-    public userID: number,
-    public username: string,
-    public email: string,
-    public password: string,
-    public roleid: number,
-    public token: string
-  ) {}
->>>>>>> e39dc45d9d0a5c9f7be088c98698c8b198b7f11f
 }
