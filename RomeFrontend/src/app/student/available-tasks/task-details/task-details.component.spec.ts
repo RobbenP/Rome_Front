677766ced2b1d869a96e3f9c31960d0c801377d6
waropdebeeck <r0709145@student.thomasmore.be>
2019-12-11 19:28:04 +0100

    fixes

--- RomeFrontend/src/app/student/available-tasks/task-details/task-details.component.spec.ts
+++ RomeFrontend/src/app/student/available-tasks/task-details/task-details.component.spec.ts
@@ -1,55 +1,25 @@
-import { Component, OnInit } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-import { Assignment } from "src/app/models/assignment.model";
-import { AssignmentService } from "src/app/services/assignment.service";
-import { CompanyService } from "src/app/services/company.service";
-import { Company } from "src/app/models/company.model";
-import { Tag } from "src/app/models/tag.model";
-import { Review } from "src/app/models/review.model";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: "app-task-details",
-  templateUrl: "./task-details.component.html",
-  styleUrls: ["./task-details.component.css"]
-})
-export class TaskDetailsComponent implements OnInit {
-  assignmentId: number;
-  assignment: Assignment;
-  approvedUserAmount: number;
-  company: Company;
-  tags: Tag[];
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private assignService: AssignmentService,
-    private companyService: CompanyService
-  ) {
-    localStorage.setItem("refreshed", "1");
-    this.route.queryParams.subscribe(p => {
-      this.assignmentId = p["assignmentId"];
-      //console.log(p["assignmentId"]);
+import { TaskDetailsComponent } from './task-details.component';
 
-      assignService.getAssignement(this.assignmentId).subscribe(r => {
-        this.assignment = r;
-        console.log(r);
+describe('TaskDetailsComponent', () => {
+  let component: TaskDetailsComponent;
+  let fixture: ComponentFixture<TaskDetailsComponent>;
 
-        companyService.getCompany(this.assignment.companyID).subscribe(r => {
-          this.company = r;
-          console.log(r);
-        });
-      });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TaskDetailsComponent ]
+    })
+    .compileComponents();
+  }));
 
-      assignService.getApprovedUsersAmount(this.assignmentId).subscribe(r => {
-        this.approvedUserAmount = r;
-        //console.log(r);
-      });
-      assignService.getTags(this.assignmentId).subscribe(r => {
-        this.tags = r;
-        //console.log(r);
-      });
-     
-    });
-  }
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TaskDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  ngOnInit() {}
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
