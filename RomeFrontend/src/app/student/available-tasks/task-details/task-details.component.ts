50786d4984c555045add60a8c24aefd0e4c29c0b
Robben P <robben.praet@gmail.com>
2019-12-11 23:58:54 +0100

    details opdracht hoe voller de opdracht is hoe roder ze wordt, als ze vol is kan een student niet meer inschrijven

--- RomeFrontend/src/app/student/available-tasks/task-details/task-details.component.ts
+++ RomeFrontend/src/app/student/available-tasks/task-details/task-details.component.ts
@@ -16,15 +16,16 @@ export class TaskDetailsComponent implements OnInit {
   assignmentId: number;
   assignment: Assignment;
   approvedUserAmount: number;
+  canSignup:boolean = true;
   company: Company;
   tags: Tag[];
+  hue: number;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private assignService: AssignmentService,
     private companyService: CompanyService
   ) {
-    localStorage.setItem("refreshed", "1");
     this.route.queryParams.subscribe(p => {
       this.assignmentId = p["assignmentId"];
       //console.log(p["assignmentId"]);
@@ -41,18 +42,54 @@ export class TaskDetailsComponent implements OnInit {
 
       assignService.getApprovedUsersAmount(this.assignmentId).subscribe(r => {
         this.approvedUserAmount = r;
+        var inverse: number = (1 - r / this.assignment.quantityUsers);
+        this.hue = inverse*120;
+        if(inverse===1)this.canSignup=false;
         //console.log(r);
       });
       assignService.getTags(this.assignmentId).subscribe(r => {
         this.tags = r;
         //console.log(r);
       });
-     
     });
   }
-
+  getHue() {
+    console.log("getHue wordt opgeroepen");
+    console.log(this.hue);
+    //return this.hue;
+    return this.hslToHex(this.hue, 100, 50);
+  }
   signup() {
     this.assignService.userAcceptAssignmentByAssignmentID(this.assignmentId);
+  }
+
+  hslToHex(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
   }
 
   ngOnInit() {}
