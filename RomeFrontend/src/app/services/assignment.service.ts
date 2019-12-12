db04668a86573870139b72813d9a7e8367cf92ea
ArneVDE <r0704140@student.thomasmore.be>
2019-12-12 17:09:30 +0100

    Taak toevoegen (put request unauthorized)

--- RomeFrontend/src/app/services/assignment.service.ts
+++ RomeFrontend/src/app/services/assignment.service.ts
@@ -32,12 +32,6 @@ export class AssignmentService {
     );
   }
 
-  getAssignementWijzig(assignId: number): Observable<Assignment[]> {
-    return this.http.get<Assignment[]>(
-      "https://localhost:5001/api/Assignments/" + assignId
-    );
-  }
-
   getApprovedUsersAmount(assignId: number): Observable<number> {
     return this.http.get<number>(
       "https://localhost:5001/api/Assignments/makersAmount/" + assignId
