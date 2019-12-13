b834c60433cd2fcc3cd232bef992a56f46dc28d2
Robben P <robben.praet@gmail.com>
2019-12-13 22:32:40 +0100

    form validatie bij registreer student

--- RomeFrontend/src/app/register/register-student/register-student.component.ts
+++ RomeFrontend/src/app/register/register-student/register-student.component.ts
@@ -1,38 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Student } from 'src/app/models/student.model';
-import { AuthenticateService } from 'src/app/services/authenticate.service';
-import { Router } from '@angular/router';
-import { User } from 'src/app/models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { Student } from "src/app/models/student.model";
+import { AuthenticateService } from "src/app/services/authenticate.service";
+import { Router } from "@angular/router";
+import { User } from "src/app/models/user.model";
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  FormControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-register-student',
-  templateUrl: './register-student.component.html',
-  styleUrls: ['./register-student.component.css']
+  selector: "app-register-student",
+  templateUrl: "./register-student.component.html",
+  styleUrls: ["./register-student.component.css"]
 })
 export class RegisterStudentComponent implements OnInit {
   datum = new Date();
-  model: Student= new Student(0,"","",this.datum,"","","","");
+  model: Student = new Student(0, "", "", this.datum, "", "", "", "");
   user: User;
-  constructor(private _authenticateService: AuthenticateService,private router: Router) { }
-  onSubmit(){
-   
-    console.log(this.model.birthDay);
-    this._authenticateService.addStudent(this.model).subscribe(
-      result =>{
-        this.model = result;
-        this._authenticateService.getUser(Number(localStorage.getItem("userID"))).subscribe(
-          result =>{
-            this.user = result;
-            this.user.studentID = this.model.studentID;
-            this._authenticateService.updateUser(this.user).subscribe();
-            this.router.navigate(['']);
-          }
-        )
-      }
-    );
-    
-  }
-  ngOnInit() {
-  }
+  constructor(
+    private _authenticateService: AuthenticateService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
+  registerForm = this.fb.group({
+    nickname: ["", Validators.required],
+    name: ["", Validators.required],
+    birthDay: ["", Validators.required],
+    biography:[""],
+    linkedIn:[""],
+    experience:[""],
+    phonenumber:[""]
+  });
+
+  get f() {
+    return this.registerForm.controls;
+  }
+  onSubmit() {
+    console.log(this.registerForm.value);
+    this._authenticateService.addStudent(this.registerForm.value).subscribe(result => {
+      this.model = result;
+      this._authenticateService
+        .getUser(Number(localStorage.getItem("userID")))
+        .subscribe(result => {
+          this.user = result;
+          this.user.studentID = this.model.studentID;
+          this._authenticateService.updateUser(this.user).subscribe();
+          this.router.navigate([""]);
+        });
+    });
+  }
+  ngOnInit() {}
 }
--- RomeFrontend/src/app/register/register.module.ts
+++ RomeFrontend/src/app/register/register.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
-import { RegisterStudentComponent } from './register-student/register-student.component';
-import { RegisterCompanyComponent } from './register-company/register-company.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RegisterComponent } from "./register/register.component";
+import { RegisterStudentComponent } from "./register-student/register-student.component";
+import { RegisterCompanyComponent } from "./register-company/register-company.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [RegisterComponent, RegisterStudentComponent, RegisterCompanyComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    RegisterComponent,
+    RegisterStudentComponent,
+    RegisterCompanyComponent
+  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class RegisterModule { }
+export class RegisterModule {}
