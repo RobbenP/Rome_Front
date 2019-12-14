bb123f8b2ed06df8b86224c004ae0df2cbbbcf57
Robben P <robben.praet@gmail.com>
2019-12-14 11:51:00 +0100

    inlog toont fout bij foute inlog gegevens

--- RomeFrontend/src/app/login/login/login.component.ts
+++ RomeFrontend/src/app/login/login/login.component.ts
@@ -1,52 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticateService } from 'src/app/services/authenticate.service';
-import { Userlogin } from 'src/app/models/userlogin.model';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticateService } from "src/app/services/authenticate.service";
+import { Userlogin } from "src/app/models/userlogin.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  model: Userlogin = new Userlogin('', '');
+  model: Userlogin = new Userlogin("", "");
   roleid: any;
-  constructor(private _authenticateService: AuthenticateService, private _router: Router) { }
+  fout: boolean = false;
+  constructor(
+    private _authenticateService: AuthenticateService,
+    private _router: Router
+  ) {}
 
   async onSubmit() {
-    const result = await this._authenticateService.authenticate(this.model);
+    try {
+      const result = await this._authenticateService.authenticate(this.model);
+      if (result.studentID != null) {
+        localStorage.setItem("studentID", result.studentID.toString());
+      }
+      if (result.companyID != null) {
+        localStorage.setItem("companyID", result.companyID.toString());
+      }
+    } catch (error) {
+      if (error.statusText == "Bad Request") {
+        this.fout = true;
+      }
+    }
 
-    if (result.studentID != null) {
-      localStorage.setItem("studentID", result.studentID.toString());
-    }
-    if (result.companyID != null) {
-      localStorage.setItem("companyID", result.companyID.toString());
-    }
-    
     this.roleid = Number(localStorage.getItem("roleID"));
-      if (this.roleid == 1){
-        this._router.navigate(['admin']);
-      }
-      else if (this.roleid == 2){
-        this._router.navigate(['bedrijf']);
-      }
-      else if (this.roleid == 3){
-        this._router.navigate(['student']);
-      }
+    if (this.roleid == 1) {
+      this._router.navigate(["admin"]);
+    } else if (this.roleid == 2) {
+      this._router.navigate(["bedrijf"]);
+    } else if (this.roleid == 3) {
+      this._router.navigate(["student"]);
+    }
   }
-  
+
   ngOnInit() {
-    this._authenticateService.isLoggedin$.subscribe(e=> {
+    this._authenticateService.isLoggedin$.subscribe(e => {
       this.roleid = Number(localStorage.getItem("roleID"));
-      if (this.roleid == 1){
-        this._router.navigate(['admin']);
+      if (this.roleid == 1) {
+        this._router.navigate(["admin"]);
+      } else if (this.roleid == 2) {
+        this._router.navigate(["bedrijf"]);
+      } else if (this.roleid == 3) {
+        this._router.navigate(["student"]);
       }
-      else if (this.roleid == 2){
-        this._router.navigate(['bedrijf']);
-      }
-      else if (this.roleid == 3){
-        this._router.navigate(['student']);
-      }
-    })
+    });
   }
 }
